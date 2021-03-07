c0c6811507be36254a174bde3ca34178a2fa42d0
Karlatemp <karlatemp@vip.qq.com>
2021-03-07 12:39:51 +0800

    Project init

---
+++ src/main/java/kotlin/kbr/Bc.java
@@ -0,0 +1,6 @@
+package kotlin.kbr;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Bc extends Plugin {
+}
---
+++ src/main/java/kotlin/kbr/Bk.java
@@ -0,0 +1,6 @@
+package kotlin.kbr;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Bk extends JavaPlugin {
+}
